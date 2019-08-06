57e98a64272759175d6c155ad91cad1f5e2ec38c
niwakon <s1611405@coins.tsukuba.ac.jp>
2019-08-06 09:19:09 +0900

    機器側が書き込み関数ないと思ったので書き込み関数削除

--- index.js
+++ index.js
@@ -759,8 +759,7 @@ function requestJoin(){
     .requestJoin(GID[0],GID[1],{
         from: acc,
         gas: 1000000,
-},(error,result)=>{
-})
+	},(error,result)=>{})
 }
 
 function getAuthenticatedMembers(){
@@ -863,60 +862,4 @@ function accessCamera(){
 
 }else{
         document.write('Install <a href="https://metamask.io">METAMASK</a>')
-}
-
-function onInitFs(fs) {
-	// titleでファイル名決定
-	  fs.root.getFile(title, {create: true, exclusive: false}, function(fileEntry) {
-		fileEntry.createWriter(function(fileWriter) {
-			 fileWriter.onerror = function(e) {
-				errorHandler(e)
-			  };
-			  fileWriter.onwriteend = function(e) {
-				   if (fileWriter.length === 0) {
-					// content　が書き込む内容
-					var blob = new Blob([content], {type: "text/pain"});
-					fileWriter.write(blob);
-				} else {
-					  console.log('Write completed.' + title);
-				}
-			  };
-			  fileWriter.truncate(0);
-		}, errorHandler);
-	  }, errorHandler);
-}
-
-function WriteFile(_title, _content){
-	title = _title;
-	content = _content;
-	window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
-
-	  window.requestFileSystem(window.TEMPORARY, 1*1024*1024, onInitFs, errorHandler);
-}
-
-function errorHandler(e) {
-	var msg = '';
-
-	switch (e.code) {
-		case FileError.QUOTA_EXCEEDED_ERR:
-			msg = 'QUOTA_EXCEEDED_ERR';
-			break;
-		  case FileError.NOT_FOUND_ERR:
-			msg = 'NOT_FOUND_ERR';
-			break;
-		  case FileError.SECURITY_ERR:
-			msg = 'SECURITY_ERR';
-			break;
-		  case FileError.INVALID_MODIFICATION_ERR:
-			msg = 'INVALID_MODIFICATION_ERR';
-			break;
-		  case FileError.INVALID_STATE_ERR:
-			msg = 'INVALID_STATE_ERR';
-			break;
-		  default:
-			   msg = 'Unknown Error';
-			break;
-	};
-
-	console.log('Error: ' + msg);
 }
