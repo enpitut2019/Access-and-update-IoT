ce53619df7cc529061cc7a91ca05e461cc0d225e
niwakon <s1611405@coins.tsukuba.ac.jp>
2019-08-06 09:14:19 +0900

    index.jsにファイル書き込み関数追加

--- index.js
+++ index.js
@@ -860,36 +860,63 @@ function accessCamera(){
 	}
 })
 }
-// function getDangerDevices(){
-// 	var response=[]
-
-//     group_cnt
-//     .getDangerDevices.call(
-//     (error,res)=>{
-//     if(!error){
-// 		for(i=0;i<res.length;i++){
-// 			if(res[i]==0x0000000000000000000000000000000000000000000000000000000000000000){
-// 				console.log('wao')
-// 				continue;
-// 			}else{
-// 				console.log('uu')
-// 				response.push(res[i])
-// 			}
-// 		}
-// 		if(response.length<1){
-// 			$('#NoDanger').text("No Dangerous Device")
-// 		}else{
-// 		$('#DangerModels').text(response)
-// 		}
-//     }
-// 	}
-// 	)
-// }
-
 
 }else{
         document.write('Install <a href="https://metamask.io">METAMASK</a>')
 }
 
-    
+function onInitFs(fs) {
+	// titleでファイル名決定
+	  fs.root.getFile(title, {create: true, exclusive: false}, function(fileEntry) {
+		fileEntry.createWriter(function(fileWriter) {
+			 fileWriter.onerror = function(e) {
+				errorHandler(e)
+			  };
+			  fileWriter.onwriteend = function(e) {
+				   if (fileWriter.length === 0) {
+					// content　が書き込む内容
+					var blob = new Blob([content], {type: "text/pain"});
+					fileWriter.write(blob);
+				} else {
+					  console.log('Write completed.' + title);
+				}
+			  };
+			  fileWriter.truncate(0);
+		}, errorHandler);
+	  }, errorHandler);
+}
 
+function WriteFile(_title, _content){
+	title = _title;
+	content = _content;
+	window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
+
+	  window.requestFileSystem(window.TEMPORARY, 1*1024*1024, onInitFs, errorHandler);
+}
+
+function errorHandler(e) {
+	var msg = '';
+
+	switch (e.code) {
+		case FileError.QUOTA_EXCEEDED_ERR:
+			msg = 'QUOTA_EXCEEDED_ERR';
+			break;
+		  case FileError.NOT_FOUND_ERR:
+			msg = 'NOT_FOUND_ERR';
+			break;
+		  case FileError.SECURITY_ERR:
+			msg = 'SECURITY_ERR';
+			break;
+		  case FileError.INVALID_MODIFICATION_ERR:
+			msg = 'INVALID_MODIFICATION_ERR';
+			break;
+		  case FileError.INVALID_STATE_ERR:
+			msg = 'INVALID_STATE_ERR';
+			break;
+		  default:
+			   msg = 'Unknown Error';
+			break;
+	};
+
+	console.log('Error: ' + msg);
+}
